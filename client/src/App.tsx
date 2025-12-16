9e10516f3d3b1d4831794a7de801435ce77dc50e
iamdiwakarsingh-pixel <iamdiwakarsingh@gmail.com>
2025-12-17 02:21:09 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter"; // Ensure Router is imported from 'wouter'
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,7 +10,8 @@ import Research from "@/pages/Research";
 import Teaching from "@/pages/Teaching";
 import Contact from "@/pages/Contact";
 
-function Router() {
+// No changes needed in this function definition
+function RouterComponent() { 
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -28,7 +29,12 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        
+        {/* WRAP THE ROUTER WITH THE BASEPATH CONFIGURATION HERE */}
+        <Router basepath="/The-Theory-Lab">
+          <RouterComponent />
+        </Router>
+
       </TooltipProvider>
     </QueryClientProvider>
   );
